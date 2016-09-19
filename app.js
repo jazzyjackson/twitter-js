97aff324fc3bdb5bc918752c1af4648bde8e6079
Hannah Cain <hcain23@gmail.com>
2016-09-19 18:13:05 -0400

    on 6/10

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 var volleyball = require('volleyball');
 
+var nunjucks = require('nunjucks');
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
 app.use(volleyball);
-app.get('/', function(req, rep, next){
-    rep.send('I AM DOING SOMETHING')
+app.get('/', function(req, res, next){
+    var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
     //next();
 })
 
-app.listen(8080, function() {console.log("server listening");})
+
+app.listen(3000, function() {console.log("server listening");})
