9df95482c24474e4295e91cb090091c43d222546
Colten Jackson <Colten Jackson>
2016-09-20 11:17:34 -0400

    loaded templates and CSS

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 var volleyball = require('volleyball');
-
 var nunjucks = require('nunjucks');
+var routes = require('./routes/');
+
+
 nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
 app.use(volleyball);
-app.get('/', function(req, res, next){
-    var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-    res.render( 'index', {title: 'Hall of Fame', people: people} );
-    //next();
-})
+app.use('/', routes);
+
 
 
 app.listen(3000, function() {console.log("server listening");})
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var tweetBank = require('../tweetBank');
+
+router.get('/', function(req, res){
+  var tweets = tweetBank.list();
+  res.render( 'index', {tweets: tweets });
+})
+
+router.get('/stylesheets/style.css', function(req,res){
+  console.log(__dirname);
+  res.sendFile('/stylesheets/style.css', {root: __dirname + '/../public/'});
+})
+
+module.exports = router;
